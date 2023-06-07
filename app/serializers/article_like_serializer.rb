01e2c29b18e72ae2e6621b02b06b6448b8374f93
山本将也 <0112masaya4849@gmail.com>
2023-06-07 22:13:05 +0900

    Task.6-2 ER図に書いた各種テーブルとmodelの作成【model の作成】

---
+++ app/serializers/article_like_serializer.rb
@@ -0,0 +1,3 @@
+class ArticleLikeSerializer < ActiveModel::Serializer
+  attributes :id
+end
--- spec/requests/article_likes_spec.rb
+++ spec/requests/article_likes_spec.rb
@@ -12,9 +12,9 @@ require "rails_helper"
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/articles", type: :request do
+RSpec.describe "/article_likes", type: :request do
   # This should return the minimal set of attributes required to create a valid
-  # Article. As you add validations to Article, be sure to
+  # ArticleLike. As you add validations to ArticleLike, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
@@ -26,7 +26,7 @@ RSpec.describe "/articles", type: :request do
 
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
-  # ArticlesController, or in your router and rack
+  # ArticleLikesController, or in your router and rack
   # middleware. Be sure to keep this updated too.
   let(:valid_headers) {
     {}
@@ -34,48 +34,48 @@ RSpec.describe "/articles", type: :request do
 
   describe "GET /index" do
     it "renders a successful response" do
-      Article.create! valid_attributes
-      get articles_url, headers: valid_headers, as: :json
+      ArticleLike.create! valid_attributes
+      get article_likes_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
   describe "GET /show" do
     it "renders a successful response" do
-      article = Article.create! valid_attributes
-      get article_url(article), as: :json
+      article_like = ArticleLike.create! valid_attributes
+      get article_like_url(article_like), as: :json
       expect(response).to be_successful
     end
   end
 
   describe "POST /create" do
     context "with valid parameters" do
-      it "creates a new Article" do
+      it "creates a new ArticleLike" do
         expect {
-          post articles_url,
-               params: { article: valid_attributes }, headers: valid_headers, as: :json
-        }.to change { Article.count }.by(1)
+          post article_likes_url,
+               params: { article_like: valid_attributes }, headers: valid_headers, as: :json
+        }.to change { ArticleLike.count }.by(1)
       end
 
-      it "renders a JSON response with the new article" do
-        post articles_url,
-             params: { article: valid_attributes }, headers: valid_headers, as: :json
+      it "renders a JSON response with the new article_like" do
+        post article_likes_url,
+             params: { article_like: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
     end
 
     context "with invalid parameters" do
-      it "does not create a new Article" do
+      it "does not create a new ArticleLike" do
         expect {
-          post articles_url,
-               params: { article: invalid_attributes }, as: :json
-        }.to change { Article.count }.by(0)
+          post article_likes_url,
+               params: { article_like: invalid_attributes }, as: :json
+        }.to change { ArticleLike.count }.by(0)
       end
 
-      it "renders a JSON response with errors for the new article" do
-        post articles_url,
-             params: { article: invalid_attributes }, headers: valid_headers, as: :json
+      it "renders a JSON response with errors for the new article_like" do
+        post article_likes_url,
+             params: { article_like: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
@@ -88,28 +88,28 @@ RSpec.describe "/articles", type: :request do
         skip("Add a hash of attributes valid for your model")
       }
 
-      it "updates the requested article" do
-        article = Article.create! valid_attributes
-        patch article_url(article),
-              params: { article: new_attributes }, headers: valid_headers, as: :json
-        article.reload
+      it "updates the requested article_like" do
+        article_like = ArticleLike.create! valid_attributes
+        patch article_like_url(article_like),
+              params: { article_like: new_attributes }, headers: valid_headers, as: :json
+        article_like.reload
         skip("Add assertions for updated state")
       end
 
-      it "renders a JSON response with the article" do
-        article = Article.create! valid_attributes
-        patch article_url(article),
-              params: { article: new_attributes }, headers: valid_headers, as: :json
+      it "renders a JSON response with the article_like" do
+        article_like = ArticleLike.create! valid_attributes
+        patch article_like_url(article_like),
+              params: { article_like: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
     end
 
     context "with invalid parameters" do
-      it "renders a JSON response with errors for the article" do
-        article = Article.create! valid_attributes
-        patch article_url(article),
-              params: { article: invalid_attributes }, headers: valid_headers, as: :json
+      it "renders a JSON response with errors for the article_like" do
+        article_like = ArticleLike.create! valid_attributes
+        patch article_like_url(article_like),
+              params: { article_like: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
@@ -117,11 +117,11 @@ RSpec.describe "/articles", type: :request do
   end
 
   describe "DELETE /destroy" do
-    it "destroys the requested article" do
-      article = Article.create! valid_attributes
+    it "destroys the requested article_like" do
+      article_like = ArticleLike.create! valid_attributes
       expect {
-        delete article_url(article), headers: valid_headers, as: :json
-      }.to change { Article.count }.by(-1)
+        delete article_like_url(article_like), headers: valid_headers, as: :json
+      }.to change { ArticleLike.count }.by(-1)
     end
   end
 end
