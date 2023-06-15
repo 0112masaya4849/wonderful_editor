eb6844810f5d7abbb67ec3538b59cf7ca18882de
山本将也 <0112masaya4849@gmail.com>
2023-06-15 22:14:31 +0900

    Task.7-3 記事一覧の実装【Article に関する API 実装】

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
+    #render json: users, each_serializer: UserSerializer
   end
 
   # GET /articles/1
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :articles
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :article
     end
   end
 
 
-  mount_devise_token_auth_for "User", at: "auth"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
